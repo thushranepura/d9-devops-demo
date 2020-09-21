b96defd0a0990f8dbbd05f4d8fe6f2c28f13c4d7
Thushara Ranepura <thushara.ranepura@oakton.com.au>
2020-09-21 11:49:30 +1000

    Added config split for env based configs

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -814,6 +814,7 @@ $databases['default']['default'] = array (
    }
 }
 
+
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
  }
